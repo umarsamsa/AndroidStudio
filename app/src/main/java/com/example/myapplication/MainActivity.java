6287c73db5634f8c2e22a7787e914bb8a4b328c1
Umar Samsa <umarsamsa34@gmail.com>
2023-02-11 17:40:17 +0800

    Initial MyApplication project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG ="MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "This is a verbose log.");
+        Log.d(TAG, "This is a debug log.");
+        Log.i(TAG, "This is an info log.");
+        Log.w(TAG, "This is a warn log.");
+        Log.e(TAG, "This is an error log.");
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+            }
+        });
     }
 }
