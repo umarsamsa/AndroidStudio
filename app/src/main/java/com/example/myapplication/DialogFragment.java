d49d736930fd5c2a91e4b5ca6e2c7da5433c5a83
Umar Samsa <umarsamsa34@gmail.com>
2023-03-18 17:46:35 +0800

    March 18, 2023

---
+++ app/src/main/java/com/example/myapplication/DialogFragment.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogFragment extends androidx.fragment.app.DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_dialog, container, false);
+    }
+}
